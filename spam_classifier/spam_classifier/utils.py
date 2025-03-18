c8fa473c662a8ce2afbc7e5ad6d2a9a6c0da9520
jeremias-mena <menajeremias08@gmail.com>
2025-03-18 17:52:56 -0300

    update(utils): change tokenize and label_spam_column methods from Utils

--- spam_classifier/spam_classifier/utils.py
+++ spam_classifier/spam_classifier/utils.py
@@ -5,12 +5,12 @@ class Utils:
     def __init__(self):
         pass
 
-    def tokenize(text:str) -> Set[str]:
+    def tokenize(self, text:str) -> Set[str]:
         text = text.lower()
         all_words = re.findall("[a-z0-9']+", text)
         return set(all_words)
     
-    def label_spam_column(value:str) -> bool:
+    def label_spam_column(self, value:str) -> bool:
         if value.lower() == 'spam': 
             return True
         else:
