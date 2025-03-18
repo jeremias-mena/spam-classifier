99429e38464385e15c2df87dc0166f89b542d1c9
jeremias-mena <menajeremias08@gmail.com>
2025-03-18 15:58:09 -0300

    update(__init__): add new modules

--- homemade-NB/homemade-NB/__init__.py
+++ homemade-NB/homemade-NB/__init__.py
@@ -1,3 +1,9 @@
 from .config import Config
+from .model import NaiveBayesClf
+from .utils import Utils
+from .data_manipulation import DataFrameOperations
 
-__all__ = ['Config']
+__all__ = ['Config',
+           'NaiveBayesClf',
+           'Utils',
+           'DataFrameOperations']
