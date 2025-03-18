bd233cb2fec8254fee43fdad28e86b8e959acd45
jeremias-mena <menajeremias08@gmail.com>
2025-03-18 16:03:11 -0300

    update(__init__): add new class Message from .utils

--- homemade-NB/homemade-NB/__init__.py
+++ homemade-NB/homemade-NB/__init__.py
@@ -1,9 +1,10 @@
 from .config import Config
 from .model import NaiveBayesClf
-from .utils import Utils
+from .utils import Utils, Message
 from .data_manipulation import DataFrameOperations
 
 __all__ = ['Config',
            'NaiveBayesClf',
            'Utils',
+           'Message',
            'DataFrameOperations']
