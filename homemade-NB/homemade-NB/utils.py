6a9f3a1577b344ae80ce9c4f98a22f644ab1b521
jeremias-mena <menajeremias08@gmail.com>
2025-03-18 11:49:42 -0300

    update(utils): add new method to Utils

--- homemade-NB/homemade-NB/utils.py
+++ homemade-NB/homemade-NB/utils.py
@@ -1,4 +1,11 @@
+from typing import Set
+import re
+
 class Utils:
     def __init__(self):
         pass
-    
+
+    def tokenize(text:str) -> Set[str]:
+        text = text.lower()
+        all_words = re.findall("[a-z0-9']+", text)
+        return set(all_words)
