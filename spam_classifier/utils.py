62089473b1a65d8fc5163c7469b2b8b87646ccb5
jeremias-mena <menajeremias08@gmail.com>
2025-03-20 15:39:50 -0300

    upate(utils): add new method split_data to class Utils

--- spam_classifier/utils.py
+++ spam_classifier/utils.py
@@ -1,5 +1,6 @@
-from typing import Set, NamedTuple
+from typing import Set, NamedTuple, List, TypeVar, Tuple
 import re
+import random
 
 class Utils:
     def __init__(self):
@@ -15,6 +16,12 @@ class Utils:
             return True
         else:
             return False
+    
+    def split_data(self, data: List[TypeVar], prob:float) -> Tuple[List[TypeVar], List[TypeVar]]:
+        data = data[:]
+        random.shuffle(data)
+        cut = int(len(data) * prob)
+        return data[:cut], data[cut:]
 
 class Message(NamedTuple):
     text: str
