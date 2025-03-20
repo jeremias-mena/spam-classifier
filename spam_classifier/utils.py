f82f0207067ff03ec88ae415d2fbdfa006bc6c40
jeremias-mena <menajeremias08@gmail.com>
2025-03-20 16:08:02 -0300

    update(utils): add new metrics method for Utils

--- spam_classifier/utils.py
+++ spam_classifier/utils.py
@@ -17,11 +17,30 @@ class Utils:
         else:
             return False
     
-    def split_data(self, data: List[TypeVar], prob:float) -> Tuple[List[TypeVar], List[TypeVar]]:
+    def split_data(self, data:List[TypeVar], prob:float) -> Tuple[List[TypeVar], List[TypeVar]]:
         data = data[:]
         random.shuffle(data)
         cut = int(len(data) * prob)
         return data[:cut], data[cut:]
+    
+    def accuracy(self, tp:int, fp:int, fn:int, tn:int) -> float:
+        correct = tp + tn
+        total = tp + fp + fn + tn
+        return correct / total
+    
+    def precision(self, tp:int, fp:int) -> float:
+        prec = tp / (tp + fp)
+        return prec
+    
+    def recall(self, tp:int, fn:int) -> float:
+        rec = tp / (tp + fn)
+        return rec
+    
+    def f1_score(self, tp:int, fp:int, fn:int, tn:int) -> float:
+        pre = self.precision(tp, fp)
+        rec = self.recall(tp, fn)
+        f1 = (2 * pre * rec) / (pre + rec)
+        return f1
 
 class Message(NamedTuple):
     text: str
